26ed4355374fbe4811271677ae2696700b5aae6a
gaburiero <gaburiero.c@gmail.com>
2015-11-15 19:02:03 -0200

    added second class examples, need to imporve and comment them

---
+++ 02aula_omp/05_omp_crit.c
@@ -0,0 +1,17 @@
+#include <omp.h>
+
+main()
+{
+
+    int x;
+    x = 0;
+
+#pragma omp parallel shared(x) 
+      {
+
+            #pragma omp critical 
+            x = x + 1;
+
+              }  /* end of parallel section */
+
+}
